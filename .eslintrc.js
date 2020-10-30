49c3fc0212dd46725dc0f1f6c576a3f37d31ea6c
Oliver Shi <oshi@yext.com>
2020-10-30 09:57:38 -0400

    Add typescript and eslint support (#1)
    This commit adds support for typescript and eslint.
    
    TEST=manual
    
    tested that it both npm run dev and npm run build will
    build the library as expected and that dev will watch changes
    tested publishing to https://www.npmjs.com/package/temp-test-answers-core
      and using that library in the sdk

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    quotes: ['error', 'single'],
+    'space-before-function-paren': ['error', {
+        named: 'never',
+        anonymous: 'never'
+    }],
+    'quote-props': ['error', 'as-needed'],
+    'max-len': ['error', { 
+        code: 100,
+        ignorePattern: '^const\\s.+=\\s*require\\s*\\('
+    }],
+    '@typescript-eslint/semi': ['error']
+  },
+  ignorePatterns: ['dist'],
+};
