00213d87702bd280ff1abe7e1119fdda2a6efaf8
Connor Anderson <canderson@yext.com>
2020-11-12 17:19:10 -0500

    update spellcheck transform

--- src/transformers/searchservice/createSpellCheck.ts
+++ src/transformers/searchservice/createSpellCheck.ts
@@ -4,6 +4,6 @@ export default function createSpellCheck(data: any): Readonly<SpellCheck> {
   return Object.freeze({
     originalQuery: data.originalQuery,
     correctedQuery: data.correctedQuery.value,
-    type: data.spellCheckType,
+    type: data.type,
   });
 }
