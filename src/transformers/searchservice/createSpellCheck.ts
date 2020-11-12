a47ae3c553a2b183e12d8166bb1b258eacafe169
Connor Anderson <canderson@yext.com>
2020-11-12 12:43:58 -0500

    and spellcheck

--- src/transformers/searchservice/createSpellCheck.ts
+++ src/transformers/searchservice/createSpellCheck.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import SpellCheck from '../../models/searchservice/response/SpellCheck';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function createSpellCheck(data: any): Readonly<SpellCheck> {
   return Object.freeze({
     originalQuery: data.originalQuery,
