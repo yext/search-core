922e4029b717ec44a075b4049a69373198d34ee0
Alexis Grow <agrow@yext.com>
2020-11-13 12:57:12 -0500

    Add vertical search request (#5)
    Add vertical search method to the SearchService along with its simple models. Additional PRs for facets, filters, and tests will follow.
    
    J=SLAP-850
    TEST=manual
    
    Run the following code (and variations to test different booleans and strings) in a jest test and inspect stdout in terminal:
    ```
    const httpRequester = new HttpRequester();
    const searchService = new SearchServiceImpl({
      apiKey: 'df4b24f4075800e5e9705090c54c6c13',
      experienceKey: 'rosetest',
      // locale: 'ja',
      v: '20191010',
      // version: 'v2.0',
    }, httpRequester);
    
    const response = await searchService.verticalSearch({
      query: 'locatoin',
      verticalKey: 'KM',
      limit: 1,
      offset: 2,
      coordinates: {
        latitude: '38.8865',
        longitude: '-77.0911'
      },
      retrieveFacets: true,
      skipSpellCheck: true,
      queryTrigger: 'query-parameter',
      sessionTrackingEnabled: true,
      sortBys: [
        {
          type: 'FIELD',
          field: 'name',
          direction: 'DESC'
        }
      ],
      context: new Context({ context: 'context' }),
      referrerPageUrl: 'http://localhost:8080',
      source: 'STANDARD'
    });
    console.log(JSON.stringify(response));
    ```

--- src/core.ts
+++ src/core.ts
@@ -1,7 +1,8 @@
 import SearchService from './services/SearchService';
 import UniversalSearchRequest from './models/searchservice/request/UniversalSearchRequest';
 import UniversalSearchResponse from './models/searchservice/response/UniversalSearchResponse';
-//import VerticalSearchResponse from './models/VerticalSearchResponse';
+import VerticalSearchRequest from './models/searchservice/request/VerticalSearchRequest';
+import VerticalSearchResponse from './models/searchservice/response/VerticalSearchResponse';
 
 export default class Core {
   constructor(private searchService: SearchService) {}
@@ -10,7 +11,7 @@ export default class Core {
     return this.searchService.universalSearch(request);
   }
 
-  /*verticalSearch(request: VerticalSearchRequest): Promise<VerticalSearchResponse> {
-    return this.searchService.verticalSearch();
-  }*/
+  verticalSearch(request: VerticalSearchRequest): Promise<VerticalSearchResponse> {
+    return this.searchService.verticalSearch(request);
+  }
 }
