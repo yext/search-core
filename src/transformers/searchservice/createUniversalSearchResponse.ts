b5c50a598dc15b0577aaafac4faa501eb4bdd4af
cea2aj <42848445+cea2aj@users.noreply.github.com>
2020-11-17 18:20:15 -0500

    Errors (#8)
    Add a simple model for the errors from the backend
    
    J=None
    TEST=manual
    
    Get an error to appear with an invalid key and ensure the response is modeled properly

--- src/transformers/searchservice/createUniversalSearchResponse.ts
+++ src/transformers/searchservice/createUniversalSearchResponse.ts
@@ -3,14 +3,19 @@ import createVerticalResults from './createVerticalResults';
 import createDirectAnswer from './createDirectAnswer';
 import createSpellCheck from './createSpellCheck';
 import createLocationBias from './createLocationBias';
+import createAnswersError from '../core/AnswersError';
 
 export default function createUniversalSearchResponse(data: any): Readonly<UniversalSearchResponse> {
   if (!data.response){
     throw new Error('The search data does not contain a response property');
   }
 
+  if (!data.meta){
+    throw new Error('The search data does not contain a meta property');
+  }
+
   const verticalResults = Array.isArray(data.response.modules)
-    ? data.response.modules.map((vertical: any) => createVerticalResults(vertical))
+    ? data.response.modules.map(createVerticalResults)
     : [];
 
   return Object.freeze({
@@ -19,6 +24,7 @@ export default function createUniversalSearchResponse(data: any): Readonly<Unive
     directAnswer: data.response.directAnswer && createDirectAnswer(data.response.directAnswer),
     searchIntents: data.response.searchIntents,
     spellCheck: data.response.spellCheck && createSpellCheck(data.response.spellCheck),
-    locationBias: data.response.locationBias && createLocationBias(data.response.locationBias)
+    locationBias: data.response.locationBias && createLocationBias(data.response.locationBias),
+    errors: data.meta.errors && data.meta.errors.map(createAnswersError)
   });
 }
