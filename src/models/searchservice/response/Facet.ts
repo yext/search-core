47cf340f2ea6fdd449a42051eeeee61da1157b05
Alexis Grow <agrow@yext.com>
2020-11-17 10:18:57 -0500

    Add facet interface

---
+++ src/models/searchservice/response/Facet.ts
@@ -0,0 +1,14 @@
+import SimpleFilter from "../request/SimpleFilter";
+
+export default interface Facet {
+  fieldId: string;
+  displayName: string;
+  options: FacetOption[];
+}
+
+export interface FacetOption {
+  displayName: string;
+  count: number;
+  selected: boolean;
+  filter: SimpleFilter;
+}
