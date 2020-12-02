b1e57b4b3f39aaa76f2fc844dabd17a0bb2488ed
Nan Hu <nhu@yext.com>
2020-12-02 11:01:11 -0500

    add comment

--- src/transformers/autocompleteservice/createAutoCompleteResponse.ts
+++ src/transformers/autocompleteservice/createAutoCompleteResponse.ts
@@ -4,12 +4,11 @@ import { createAutoCompleteResult } from './createAutoCompleteResult';
 export function createAutoCompleteResponse(response: any): Readonly<AutoCompleteResponse> {
   response = response.json();
   let results;
+  // the response may have its results nested in a sections object
   if (response.sections) {
-    results = response.sections.results.map((result: any) =>
-      createAutoCompleteResult(result));
+    results = response.sections.results.map(createAutoCompleteResult);
   } else {
-    results = response.results.map((result: any) =>
-      createAutoCompleteResult(result));
+    results = response.results.map(createAutoCompleteResult);
   }
   const inputIntents = response.input ? response.input.queryIntents : [];
   return Object.freeze({
