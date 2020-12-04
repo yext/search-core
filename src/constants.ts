a5535c0fecefa6eaf63f906d7789be7bb445131c
cea2aj <42848445+cea2aj@users.noreply.github.com>
2020-12-04 14:04:54 -0500

    Fix vertical search URL (#22)
    Fix the vertical search url which was incorrect in #20
    
    J=none
    TEST=manual
    
    Run a vertical search request and confirm that it now works

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ export const defaultApiVersion = 20190101;
 
 export const defaultEndpoints: Required<Endpoints> = {
   universalSearch: 'https://liveapi.yext.com/v2/accounts/me/answers/query',
-  verticalSearch: 'https://liveapi.yext/v2/accounts/me/answers/vertical/query',
+  verticalSearch: 'https://liveapi.yext.com/v2/accounts/me/answers/vertical/query',
   questionSubmission: 'https://api.yext.com/v2/accounts/me/createQuestion',
   status: 'https://answersstatus.pagescdn.com',
   universalAutoComplete: 'https://liveapi-cached.yext.com/v2/accounts/me/answers/autocomplete',
