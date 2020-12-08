1bcfad0d918635eb385c38bebfcd26a79446e563
Connor Anderson <canderson@yext.com>
2020-12-08 11:29:05 -0500

    rename Core to AnswersCore

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import MasterSwitchApi from './infra/MasterSwitchApi';
-import Core from './core';
+import AnswersCore from './AnswersCore';
 import SearchServiceImpl from './infra/SearchServiceImpl';
 import QuestionSubmissionServiceImpl from './infra/QuestionSubmissionServiceImpl';
 import HttpServiceImpl from './infra/HttpServiceImpl';
 import AnswersConfig from './models/core/AnswersConfig';
 import AutoCompleteServiceImpl from './infra/AutoCompleteServiceImpl';
 
-export function provideCore(config: AnswersConfig): Promise<Core> {
+export function provideCore(config: AnswersConfig): Promise<AnswersCore> {
   const masterSwitch = new MasterSwitchApi(config);
 
   return masterSwitch.isEnabled().then(isEnabled => {
@@ -17,9 +17,9 @@ export function provideCore(config: AnswersConfig): Promise<Core> {
     const searchService = new SearchServiceImpl(config, httpService);
     const questionSubmissionService = new QuestionSubmissionServiceImpl(config, httpService);
     const autoCompleteService = new AutoCompleteServiceImpl(config, httpService);
-    return new Core(searchService, questionSubmissionService, autoCompleteService);
+    return new AnswersCore(searchService, questionSubmissionService, autoCompleteService);
   });
 }
 
-export { Core };
+export { AnswersCore };
 export * from './models';
