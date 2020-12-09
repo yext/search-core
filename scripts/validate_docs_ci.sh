b187841b65924a1fb58abc086d8b294f00fd8af6
Connor Anderson <canderson@yext.com>
2020-12-09 14:05:54 -0500

    build script updates

--- scripts/validate_docs_ci.sh
+++ scripts/validate_docs_ci.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This script is meant to be run by CI to verify that the docs are up to date with the source code
-# It works by generating docs and using git diff to see if there are any changes
-# This script will also fail if the api report is out of date
+# The script should be ran after CI bulds new docs
+# If there are any diffs after new docs are generated, they must be out of date
 
-npm run build-ci
+npm run generate-docs
 git diff --exit-code
 
 diff_exit_code=$?
