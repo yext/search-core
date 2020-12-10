534c9907fbc120e09d7924c4d0fb58e4604b073e
Connor Anderson <canderson@yext.com>
2020-12-10 16:44:51 -0500

    update answers config docs

--- src/models/core/AnswersConfig.ts
+++ src/models/core/AnswersConfig.ts
@@ -8,13 +8,9 @@ import Endpoints from './Endpoints';
 export default interface AnswersConfig {
   /** The api key of the answers experience */
   apiKey: string,
-  /**
-   * The experience key of the answers experience
-   */
+  /** The experience key of the answers experience */
   experienceKey: string,
-  /**
-   * The locale of the answers experience
-   */
+  /** The locale of the answers experience */
   locale: string,
   /**
    * The version of the answers experience configuration
