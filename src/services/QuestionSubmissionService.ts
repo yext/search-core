c1bfb073f0052a47a63377b918df7da0a81f71da
Connor Anderson <canderson@yext.com>
2020-12-14 17:25:43 -0500

    update service docs

--- src/services/QuestionSubmissionService.ts
+++ src/services/QuestionSubmissionService.ts
@@ -1,6 +1,9 @@
 import { QuestionSubmissionResponse } from '../models/questionsubmission/QuestionSubmissionResponse';
 import { QuestionSubmissionRequest } from '../models/questionsubmission/QuestionSubmissionRequest';
 
+/**
+ * Submits a custom question to the Answers API.
+ */
 export interface QuestionSubmissionService {
   submitQuestion(request: QuestionSubmissionRequest): Promise<QuestionSubmissionResponse>
 }
