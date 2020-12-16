735d19e870a4d47f15e456e1ca34183aae4fd511
Tom Meyer <tmeyer@yext.com>
2020-12-16 10:44:51 -0500

    Remove the Answers status check from `provideCore`. (#37)
    We no longer need to include an Answers status check in `provideCore`. This was
    confirmed by Product. Because of this, `provideCore` can now be synchronous.
    We can also remove the `MasterSwitchApi`.
    
    J=SLAP-976
    TEST=manual
    
    Pointed the answers-core-testing repo to my local build of answers-core. I
    executed all of the methods exposed in the testing repo to ensure that the
    Core functionality still worked as expected.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { MasterSwitchApi } from './infra/MasterSwitchApi';
 import { AnswersCore } from './AnswersCore';
 import { SearchServiceImpl } from './infra/SearchServiceImpl';
 import { QuestionSubmissionServiceImpl } from './infra/QuestionSubmissionServiceImpl';
@@ -10,26 +9,19 @@ import { AutoCompleteServiceImpl } from './infra/AutoCompleteServiceImpl';
  * The entrypoint to the answers-core library.
  *
  * @remarks
- * Returns a Promise containing an {@link AnswersCore} instance.
- * If the backend determines that the core should be disabled, this function returns a rejected promise.
+ * Returns an {@link AnswersCore} instance.
  *
  * @param config - The answers-core config
  *
  * @public
  */
-export function provideCore(config: AnswersConfig): Promise<AnswersCore> {
-  const masterSwitch = new MasterSwitchApi(config);
+export function provideCore(config: AnswersConfig): AnswersCore {
+  const httpService = new HttpServiceImpl();
+  const searchService = new SearchServiceImpl(config, httpService);
+  const questionSubmissionService = new QuestionSubmissionServiceImpl(config, httpService);
+  const autoCompleteService = new AutoCompleteServiceImpl(config, httpService);
 
-  return masterSwitch.isEnabled().then(isEnabled => {
-    if (!isEnabled) {
-      throw new Error('MasterSwitchApi determined the front-end should be disabled');
-    }
-    const httpService = new HttpServiceImpl();
-    const searchService = new SearchServiceImpl(config, httpService);
-    const questionSubmissionService = new QuestionSubmissionServiceImpl(config, httpService);
-    const autoCompleteService = new AutoCompleteServiceImpl(config, httpService);
-    return new AnswersCore(searchService, questionSubmissionService, autoCompleteService);
-  });
+  return new AnswersCore(searchService, questionSubmissionService, autoCompleteService);
 }
 
 export { AnswersCore };
