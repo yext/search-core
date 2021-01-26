f9beffe2377a9c2e5138f88e324405a97020323a
Connor Anderson <canderson@yext.com>
2021-01-26 15:54:46 -0500

    add tests for disabled filters

--- tests/serializers/serializeFacetFilters.ts
+++ tests/serializers/serializeFacetFilters.ts
@@ -22,3 +22,33 @@ it('serializeFacetFilters serializes facets properly', () => {
   };
   expect(actualSerializedFilters).toEqual(JSON.stringify(expectedSerializedFilters));
 });
+
+it('serializeFacetFilters serializes disabled facets properly', () => {
+  const actualSerializedFilters = serializeFacetFilters([
+    { fieldId: 'c_jobCategory', disabled: true },
+    { fieldId: 'c_jobLocationShortDescription', disabled: true},
+  ]);
+
+  const expectedSerializedFilters = {
+    c_jobCategory: [],
+    c_jobLocationShortDescription: []
+  };
+  expect(actualSerializedFilters).toEqual(JSON.stringify(expectedSerializedFilters));
+});
+
+it('serializeFacetFilters serializes a mix of disabled and enabled filters properly', () => {
+  const actualSerializedFilters = serializeFacetFilters([
+    { fieldId: 'c_jobCategory', comparator: '$eq', comparedValue: 'Sales' },
+    { fieldId: 'c_jobCategory', comparator: '$eq', comparedValue: 'Client Success' },
+    { fieldId: 'c_jobLocationShortDescription', disabled: true},
+  ]);
+
+  const expectedSerializedFilters = {
+    c_jobCategory: [
+      { c_jobCategory: { $eq: 'Sales' } },
+      { c_jobCategory: { $eq: 'Client Success' } },
+    ],
+    c_jobLocationShortDescription: []
+  };
+  expect(actualSerializedFilters).toEqual(JSON.stringify(expectedSerializedFilters));
+});
