363a442b07ff8f5e9c399c8df881d77a076b269a
Oliver Shi <oshi@yext.com>
2021-02-04 17:13:50 -0500

    what happens if I delete a bunch of tests

--- tests/transformers/createUniversalSearchResponse.ts
+++
@@ -1,9 +0,0 @@
-import { createUniversalSearchResponse } from '../../src/transformers/searchservice/createUniversalSearchResponse';
-import liveApiResponse from '../fixtures/liveapiuniversalresponse.json';
-import coreResponse from '../fixtures/coreuniversalresponse.json';
-
-it('createUniversalSearchResponse', () => {
-  const actualCoreResponse = createUniversalSearchResponse(liveApiResponse);
-
-  expect(actualCoreResponse).toMatchObject(coreResponse);
-});
