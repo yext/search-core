c78c2fe00ebde1b79b1cdb2987ee6172ad2909eb
Oliver Shi <oshi@yext.com>
2021-02-09 19:05:59 -0500

    matcher -> comparison in docs

--- src/models/searchservice/common/Matcher.ts
+++ src/models/searchservice/common/Matcher.ts
@@ -5,42 +5,42 @@
  */
 export enum Matcher {
   /**
-   * An equals matcher.
+   * An equals comparison.
    *
    * @remarks
    * Compatible with all field types.
    */
   Equals = '$eq',
   /**
-   * A not equals matcher.
+   * A not equals comparison.
    *
    * @remarks
    * Compatible with all field types.
    */
   NotEquals = '!$eq',
   /**
-   * A less than matcher.
+   * A less than comparison.
    *
    * @remarks
    * Compatible with integer, float, date, datetime, and time fields.
    */
   LessThan = '$lt',
   /**
-   * A less than or equal to matcher.
+   * A less than or equal to comparison.
    *
    * @remarks
    * Compatible with integer, float, date, datetime, and time fields.
    */
   LessThanOrEqualTo = '$le',
   /**
-   * A greater than matcher.
+   * A greater than comparison.
    *
    * @remarks
    * Compatible with integer, float, date, datetime, and time fields.
    */
   GreaterThan = '$gt',
   /**
-   * A greater than or equal to matcher.
+   * A greater than or equal to comparison.
    *
    * @remarks
    * Compatible with integer, float, date, datetime, and time fields.
