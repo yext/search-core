851e9a428ebdd30e4d60a870130dd4fb44249cbe
Connor Anderson <canderson@yext.com>
2021-06-30 14:08:26 -0400

    add example

--- docs/answers-core.universallimit.md
+++ docs/answers-core.universallimit.md
@@ -11,3 +11,8 @@ The maximum limit of results per vertical. Each limit can be set from 1-50, incl
 ```typescript
 export interface UniversalLimit 
 ```
+
+## Example
+
+Example object: { people: 5 locations: 10 events: 8 }
+
--- src/models/searchservice/request/UniversalLimit.ts
+++ src/models/searchservice/request/UniversalLimit.ts
@@ -2,6 +2,14 @@
  * The maximum limit of results per vertical.
  * Each limit can be set from 1-50, inclusive.
  *
+ * @example
+ * Example object:
+ * {
+ *    people: 5
+ *    locations: 10
+ *    events: 8
+ * }
+ *
  * @public
  */
 export interface UniversalLimit {
