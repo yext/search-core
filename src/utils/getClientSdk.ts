6c4b85dd92452b062bf2f3adaea30bba61362448
nmanu1 <nmanu@yext.com>
2022-03-15 17:26:10 -0400

    Fix spacing

--- src/utils/getClientSdk.ts
+++ src/utils/getClientSdk.ts
@@ -7,9 +7,11 @@ export function getClientSdk(customClientSdk?: CustomClientSdk): ClientSdk {
   const coreAgent = { ANSWERS_CORE: version };
 
   return Object.entries(customClientSdk ?? {}).reduce((clientSdk, [agent, version]) => {
-    return version ? {
-      ...clientSdk,
-      [agent]: version
-    } : clientSdk;
+    return version
+      ? {
+        ...clientSdk,
+        [agent]: version
+      }
+      : clientSdk;
   }, coreAgent);
 }
