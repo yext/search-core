933cc1274e462c288db647315b557644d9e5db06
Oliver Shi <oshi@yext.com>
2022-05-25 13:02:08 -0400

    make const

--- src/constants.ts
+++ src/constants.ts
@@ -25,4 +25,4 @@ export const SandboxEndpoints: Required<Endpoints> = {
   universalAutocomplete: 'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete',
   verticalAutocomplete: 'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete',
   filterSearch: 'https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch',
-}
+} as const;
