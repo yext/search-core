f095115696524ac7971156ac4ca1c34e5f2e698e
Oliver Shi <oshi@yext.com>
2022-05-26 11:25:33 -0400

    fix tests

--- tests/infra/AutocompleteServiceImpl.ts
+++ tests/infra/AutocompleteServiceImpl.ts
@@ -1,5 +1,5 @@
 import { HttpServiceMock } from '../mocks/HttpServiceMock';
-import { AnswersConfig } from '../../src/models/core/AnswersConfig';
+import { AnswersConfigWithDefaulting } from '../../src/models/core/AnswersConfig';
 import {
   UniversalAutocompleteRequest,
   VerticalAutocompleteRequest,
@@ -16,28 +16,32 @@ import { ApiResponseValidator } from '../../src/validation/ApiResponseValidator'
 import { ApiResponse } from '../../src/models/answersapi/ApiResponse';
 import { AnswersError } from '../../src/models/answersapi/AnswersError';
 import { getClientSdk } from '../../src/utils/getClientSdk';
-import { SandboxEndpoints } from '../../src/constants';
 
 describe('AutocompleteService', () => {
-  const config: AnswersConfig = {
+  const config: AnswersConfigWithDefaulting = {
     apiKey: 'testApiKey',
     experienceKey: 'testExperienceKey',
     locale: 'en',
     visitor: {
       id: '123',
       idMethod: 'YEXT_AUTH'
-    }
+    },
+    endpoints: defaultEndpoints
   };
 
-  const configWithToken: AnswersConfig = {
+  const configWithToken: AnswersConfigWithDefaulting = {
     token: 'testToken',
     experienceKey: 'testExperienceKey',
     locale: 'en',
+    endpoints: defaultEndpoints
   };
 
   const mockHttpService = new HttpServiceMock();
   const apiResponseValidator = new ApiResponseValidator();
-  function createMockAutocompleteService(params?: { response?: ApiResponse, answersConfig?: AnswersConfig }) {
+  function createMockAutocompleteService(params?: {
+    response?: ApiResponse,
+    answersConfig?: AnswersConfigWithDefaulting
+  }) {
     const {
       response = mockAutocompleteResponse,
       answersConfig = config
@@ -243,13 +247,14 @@ describe('AutocompleteService', () => {
 });
 
 describe('additionalQueryParams are passed through', () => {
-  const config: AnswersConfig = {
+  const config: AnswersConfigWithDefaulting = {
     apiKey: 'testApiKey',
     experienceKey: 'testExperienceKey',
     locale: 'en',
     additionalQueryParams: {
       jsLibVersion: 'LIB_VERSION'
-    }
+    },
+    endpoints: defaultEndpoints
   };
   let mockHttpService, apiResponseValidator, autocompleteService;
   beforeEach(() => {
