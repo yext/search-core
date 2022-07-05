1d73c4bddd8fe5c44ae5efa3658e8da05346036d
Alex Taing <ataing@yext.com>
2022-07-05 15:59:40 -0400

    rename AnswersCore to SearchCore

---
+++ src/deprecated.ts
@@ -0,0 +1,7 @@
+import { SearchCore } from './SearchCore';
+
+/**
+ * @deprecated AnswersCore is deprecated and has been replaced by SearchCore
+ * @public
+ */
+export class AnswersCore extends SearchCore{}
