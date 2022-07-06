39bb54b6d506025872439ffca3fd47493112a1b4
Alex Taing <ataing@yext.com>
2022-07-06 09:25:57 -0400

    linked deprecated with new terms

--- src/models/deprecated.ts
+++ src/models/deprecated.ts
@@ -2,13 +2,13 @@ import { SearchError } from './answersapi/SearchError';
 import { SearchRequest } from './core/SearchRequest';
 
 /**
- * @deprecated AnswersError is deprecated and has been replaced by SearchError
+ * @deprecated AnswersError is deprecated and has been replaced by {@link SearchError}
  * @public
  */
 export type AnswersError = SearchError;
 
 /**
-* @deprecated AnswersRequest is deprecated and has been replaced by SearchRequest
+* @deprecated AnswersRequest is deprecated and has been replaced by {@link SearchRequest}
 * @public
 */
 export type AnswersRequest = SearchRequest;
