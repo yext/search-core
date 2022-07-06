0691e81a3dff89236c1921f70f407854575ee867
Alex Taing <ataing@yext.com>
2022-07-06 09:53:34 -0400

    link deprecated with new symbols

--- src/models/deprecated.ts
+++ src/models/deprecated.ts
@@ -4,25 +4,25 @@ import { SearchConfigWithApiKey } from './core/SearchConfig';
 import { SearchConfigWithToken } from './core/SearchConfig';
 
 /**
- * @deprecated AnswersConfig is deprecated and has been replaced by SearchConfig
+ * @deprecated AnswersConfig is deprecated and has been replaced by {@link SearchConfig}
  * @public
  */
 export type AnswersConfig = SearchConfig;
 
 /**
- * @deprecated BaseAnswersConfig is deprecated and has been replaced by BaseSearchConfig
+ * @deprecated BaseAnswersConfig is deprecated and has been replaced by {@link BaseSearchConfig}
  * @public
  */
 export type BaseAnswersConfig = BaseSearchConfig;
 
 /**
- * @deprecated AnswersConfigWithApiKey is deprecated and has been replaced by SearchConfigWithApiKey
+ * @deprecated AnswersConfigWithApiKey is deprecated and has been replaced by {@link SearchConfigWithApiKey}
  * @public
  */
 export type AnswersConfigWithApiKey = SearchConfigWithApiKey;
 
 /**
- * @deprecated AnswersConfigWithToken is deprecated and has been replaced by SearchConfigWithToken
+ * @deprecated AnswersConfigWithToken is deprecated and has been replaced by {@link SearchConfigWithToken}
  * @public
  */
 export type AnswersConfigWithToken = SearchConfigWithToken;
