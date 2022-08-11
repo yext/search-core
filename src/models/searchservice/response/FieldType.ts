a1ecd60123603ae1118f03438ad2cb157c36bffa
juliannzhou <65748924+juliannzhou@users.noreply.github.com>
2022-08-11 11:09:49 -0400

    comment out unused enum fields

--- src/models/searchservice/response/FieldType.ts
+++ src/models/searchservice/response/FieldType.ts
@@ -4,19 +4,19 @@
  * @public
  */
 export enum FieldType {
-  URL = 'url',
-  ComplexURL = 'complex_url',
-  IOSAppURL = 'ios_app_url',
-  AndroidAppURL = 'android_app_url',
-  FacebookURL = 'facebook_url',
-  Email = 'email',
-  InstagramHandle = 'instagram_handle',
-  TwitterHandle = 'twitter_handle',
-  Phone = 'phone',
-  Address = 'address',
-  Hours = 'hours',
-  Decimal = 'decimal',
-  RichText = 'rich_text',
-  SingleLineText = 'single_line_text',
-  MultiLineText = 'multi_line_text'
+  // URL = 'url',
+  // ComplexURL = 'complex_url',
+  // IOSAppURL = 'ios_app_url',
+  // AndroidAppURL = 'android_app_url',
+  // FacebookURL = 'facebook_url',
+  // Email = 'email',
+  // InstagramHandle = 'instagram_handle',
+  // TwitterHandle = 'twitter_handle',
+  // Phone = 'phone',
+  // Address = 'address',
+  // Hours = 'hours',
+  // Decimal = 'decimal',
+  // RichText = 'rich_text',
+  // SingleLineText = 'single_line_text',
+  // MultiLineText = 'multi_line_text'
 }
