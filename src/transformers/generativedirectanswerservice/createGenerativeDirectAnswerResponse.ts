6127466bc6b462bc60cecd981240a99cf9e50cf1
Anh Thu Nguyen <anguyen@yext.com>
2024-06-04 12:19:02 -0400

    removed empty response check
    should have been covered in api response validator

--- src/transformers/generativedirectanswerservice/createGenerativeDirectAnswerResponse.ts
+++ src/transformers/generativedirectanswerservice/createGenerativeDirectAnswerResponse.ts
@@ -1,12 +1,6 @@
 import { GenerativeDirectAnswerResponse } from '../../models/generativedirectanswer/GenerativeDirectAnswerResponse';
 
 export function createGenerativeDirectAnswerResponse(data: any): GenerativeDirectAnswerResponse {
-  if (!data.response) {
-    throw new Error('The direct answer data does not contain a response property');
-  }
-  if (!Object.keys(data.response).length) {
-    throw new Error('The direct answer response is empty');
-  }
   return {
     directAnswer: data.response.directAnswer,
     resultStatus: data.response.resultStatus,
