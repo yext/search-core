88ee0e7b1d4e34a32da65c9c86f422e2227273ab
Victoria DaRosa <vdarosa@yext.com>
2024-07-12 13:12:43 -0400

    search-core: Adding Page Number to Segment
    Added an optional parameter called pageNumber to Segments.ts

--- src/models/searchservice/response/Segment.ts
+++ src/models/searchservice/response/Segment.ts
@@ -7,5 +7,7 @@ export interface Segment {
   /** The value of the segment as plain text. */
   text: string,
   /** The similarity score of the segment from 0 to 1 */
-  score: number
+  score: number,
+  /** The page number of a segment from a pdf */
+  pageNumber?: number
 }
